92dfbab9c84f2eaf87ab00eca62356b223ca4a43
James Horrocks <jphorrocks@proton.me>
2025-03-24 06:04:42 +0000

    changed ghostty theme

--- hosts/nixps/home_manager/ghostty.nix
+++ hosts/nixps/home_manager/ghostty.nix
@@ -6,7 +6,7 @@
     enableZshIntegration = true;
 
     settings = {
-      theme = "dracula";
+      theme = "noctis-uva";
       font-family = "MesloLGS NF";
       font-size = 14;
 
@@ -19,5 +19,17 @@
       shell-integration = "zsh";
       shell-integration-features = true;
     };
+  };
+
+  home.file = {
+    ".config/ghostty/themes/noctis".source = ./ghostty_themes/noctis;
+    ".config/ghostty/themes/noctis-azureus".source = ./ghostty_themes/noctis-azureus;
+    ".config/ghostty/themes/noctis-bordo".source = ./ghostty_themes/noctis-bordo;
+    ".config/ghostty/themes/noctis-hibernus".source = ./ghostty_themes/noctis-hibernus;
+    ".config/ghostty/themes/noctis-lilac".source = ./ghostty_themes/noctis-lilac;
+    ".config/ghostty/themes/noctis-lux".source = ./ghostty_themes/noctis-lux;
+    ".config/ghostty/themes/noctis-minimus".source = ./ghostty_themes/noctis-minimus;
+    ".config/ghostty/themes/noctis-uva".source = ./ghostty_themes/noctis-uva;
+    ".config/ghostty/themes/noctis-viola".source = ./ghostty_themes/noctis-viola;
   };
 }
