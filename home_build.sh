a26a27edab2b550d7ece1517726a9001ca62ab4e
James Horrocks <james.horrocks@ensono.com>
2025-04-23 08:56:21 +0100

    added coreutils to wsl and fixed home_build.sh

--- home_build.sh
+++ home_build.sh
@@ -1,6 +1,6 @@
 nh --version
 if [ $? -eq 0 ]; then
-    cachix watch-exec --watch-mode post-build-hook --compression-level 10 james-horrocks -- nh home switch -b bak
+    cachix watch-exec --watch-mode post-build-hook --compression-level 10 james-horrocks -- nh home switch --configuration $1 -b bak .
 else
     cachix watch-exec --watch-mode post-build-hook --compression-level 10 james-horrocks -- home-manager --flake .#$1 -b bak switch
 fi
