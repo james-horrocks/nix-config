12b9045b04a0550298fa46350b1e400fe3a83571
James Horrocks <15187219+james-horrocks@users.noreply.github.com>
2025-04-24 20:34:40 +0100

    Update home_build.sh

--- home_build.sh
+++ home_build.sh
@@ -1,6 +1,14 @@
-nh --version
-if [ $? -eq 0 ]; then
-    cachix watch-exec --watch-mode post-build-hook --compression-level 10 james-horrocks -- nh home switch --configuration $1 -b bak .
+if command -v cachix &>/dev/null; then
+    cachix authtoken $(op read op://personal/cachix/personal\ auth\ token)
+    if nh --version &>/dev/null; then
+        cachix watch-exec --watch-mode post-build-hook --compression-level 10 james-horrocks -- nh home switch --configuration $1 -b bak .
+    else
+        cachix watch-exec --watch-mode post-build-hook --compression-level 10 james-horrocks -- home-manager --flake .#$1 -b bak switch
+    fi
 else
-    cachix watch-exec --watch-mode post-build-hook --compression-level 10 james-horrocks -- home-manager --flake .#$1 -b bak switch
+    if nh --version &>/dev/null; then
+        nh home switch --configuration $1 -b bak .
+    else
+        home-manager --flake .#$1 -b bak switch
+    fi
 fi
